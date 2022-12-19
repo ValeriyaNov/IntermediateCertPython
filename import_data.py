4aeb1db70cf801d6c7cafcd2245b5ad2b8167d40
Valeriya <wal.now@yandex.ru>
2022-12-19 13:49:33 +0300

    i

--- import_data.py
+++ import_data.py
@@ -3,35 +3,37 @@ import json
 import os
 os.chdir(os.path.dirname(__file__))
 
+'''
+Здесь мы считываем и сохраняем, в нашу базу, записи из файла, 
+который указал пользователь
+'''
 
 
-
-# def import_csv(file_loger):
-#     # https://pythonworld.ru/moduli/modul-csv.html
-#     with open(path, "a", newline='', encoding='utf-8') as file:
-#         fieldnames = ['Имя','Фамилия','Отчество','Дата рождения','Номер телефона','Email']
-#         writer = csv.DictWriter(file, fieldnames=fieldnames)
-#         writer.writeheader()
-#         writer.writerow(file_loger)
-#         # получается такой вид в файле:
-#         # Карала,Каронарс,Каонмморвна,1.1.1000,123456789,xdfb@zdvb.com
-#         print('Данные успешно сохранены в csv')
-
-
-# def import_json(file_loger):
-#     with open("import.json", "a", encoding='utf-8') as file:
-#         json.dump(file_loger, file, indent=2, ensure_ascii=False)
-#         file.write(',\n')
-#         print('Данные успешно сохранены в json')
+def import_csv(file_loger):
+    # https://pythonworld.ru/moduli/modul-csv.html
+    with open('import.csv', "a", newline='', encoding='utf-8') as file:
+        fieldnames = ['Имя','Фамилия','Отчество','Дата рождения','Номер телефона','Email']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow(file_loger)
+        # получается такой вид в файле:
+        # Карала,Каронарс,Каонмморвна,1.1.1000,123456789,xdfb@zdvb.com
+        print('Данные успешно сохранены в csv')
 
 
-# код от Валерии
+def import_json(file_loger):
+    with open("import.json", "a", encoding='utf-8') as file:
+        json.dump(file_loger, file, indent=2, ensure_ascii=False)
+        file.write(',\n')
+        print('Данные успешно сохранены в json')
+
+
 def copy_cont():
     path = input('Введите файл, откуда хотите скопировать данные   ')
     def import_csv(path_to_import_csv_file): 
         data = []        
         with open(path_to_import_csv_file, "r", newline='', encoding='utf-8') as file:
-        
+
             file_reader = csv.reader(file, delimiter = ",") 
             for row in file_reader:
                 data.append(row)     
@@ -56,7 +58,7 @@ def copy_cont_json():
                 g = list(file_reader[i].values())
                 data.append(g)
         return data
-    
+
     d = import_json(path)
     new = input('Введите имя или фаммилию человека, данные которого хотите записать')
     arr = []
@@ -70,25 +72,3 @@ def copy_cont_json():
             csv_fil.writerow(arr[i])
 
 
-
-
-
-
-
-
-
-'''
-def import_json(file_loger):
-    data = []      
-    with open('import.json', 'r', encoding='UTF-8') as file: 
-        data = json.load(file) 
-        for i in range(0, len(data)): 
-            d1 = {'contact_id': ''}
-            data[i], d1 = d1, data[i]
-            data[i].update(d1)
-    #return data
-'''
-
-
-
-   
