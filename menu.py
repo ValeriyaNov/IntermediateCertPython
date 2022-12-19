17e15bcba6d70a341720c3cea2b641aaeb73b80c
Valeriya <wal.now@yandex.ru>
2022-12-19 13:49:53 +0300

    m

--- menu.py
+++ menu.py
@@ -1,6 +1,5 @@
-import controll
-import os
-os.chdir(os.path.dirname(__file__))
+import controller
+
 
 def greeting():
     print()
@@ -11,16 +10,16 @@ def menu():
     while True:
         print('\nМЕНЮ')
         print('1. Добавить новую запись')  # Работает
-        print('2. Вывод записей на экран')  # выводит в терминал
-        print('3. Скопировать данные в файл?')  # сделала копирование из csv in csv
-        print('4. Сохранить в нашу базу данных?') # сделано
+        print('2. Вывод записей на экран')  # Работает
+        print('3. Скопировать данные в файл?')  # надо добавить копирование в json
+        print('4. Сохранить в нашу базу данных?') # надо сделать
         print('5. Поиск записи')  # работает
         print('6. Выход\n')   # работает
         number = input('Выберите пункт меню: ')
-        controll.distrib(number)
-         
+        controller.distribute(number)
         if number =='6':
             break
  
+
 greeting()
 menu()
