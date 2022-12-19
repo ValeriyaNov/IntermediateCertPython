4d484c3203f415fd14738618c28b9db6fa4f97bd
Valeriya <wal.now@yandex.ru>
2022-12-19 13:50:11 +0300

    o

--- operations.py
+++ operations.py
@@ -1,13 +1,10 @@
-#import controller
+import csv
 import import_data
 import json
-import csv
 import os
 os.chdir(os.path.dirname(__file__))
 
-
-
-# Создаем новый контакт, сохраняем в loger.txt
+# Создаем новый контакт
 def new_contact():
     first_name = input_firstname()
     last_name = input_lastname()
@@ -15,10 +12,6 @@ def new_contact():
     description = input_description()
     new_list = [first_name, last_name, phone_number, description]
     save_to_csv(new_list)
-    #contact_details = {'Имя': first_name, 'Фамилия': last_name, 
-    #                   'Отчество': patronymic,
-    #                   'Дата рождения': date_of_birth, 'Номер телефона':
-    #                   phone_number, 'Email': mail}
     print('Данные успешно сохранены')
 
 
@@ -38,6 +31,7 @@ def input_lastname():
     return firstchar.upper() + la_name
 
 
+#  Вводим описание
 def input_description():
     first = input('Описание: ')
     pa_name = first[1:]
@@ -45,9 +39,9 @@ def input_description():
     return firstchar.upper() + pa_name
 
 
-# Код от Александра
-def save_to_csv(new_contacts):
-    with open('phonebook.csv', 'a', encoding='utf-8') as bd:
+# Сохраняем в csv
+def save_to_csv(new_contacts, file_name = 'phonebook.csv'):
+    with open(file_name, "a", newline='', encoding='utf-8') as bd:
         for i in range(len(new_contacts)):
             if i != len(new_contacts) - 1:
                 bd.write(f'{new_contacts[i]};')
@@ -56,9 +50,9 @@ def save_to_csv(new_contacts):
         bd.write('\n')
 
 
-# Код от Александра
+# Читаем csv
 def read_contact(unit = 1, file_name = 'phonebook.csv'):
-    with open(file_name, newline = '', encoding="utf-8") as f:
+    with open(file_name, "r", newline='', encoding='utf-8') as f:
         reader = csv.reader(f, delimiter=';')
         for row in reader:
             if unit == 2:
@@ -68,4 +62,3 @@ def read_contact(unit = 1, file_name = 'phonebook.csv'):
                 for item in row:
                     print(item)
                 print()
-
