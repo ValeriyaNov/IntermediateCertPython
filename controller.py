804d3560c9e8add9d5328cb44de0cdbcfb53ed3f
Valeriya <wal.now@yandex.ru>
2023-04-09 00:35:11 +0300

    d

--- controller.py
+++ controller.py
@@ -27,50 +27,11 @@ def distribute(n):
         print('Выберите формат экспорта данных:', '1 - xml', '2 - json', '3 - csv', sep='\n')
         num_exp = input()
         if num_exp == '1':
-            export_data.export_to_xml(file_name_expott)
-            print('Данные успешно экспортированы в формате xml!\n')
-        elif num_exp == '2':
             export_data.csv_to_json(file_name_expott)
             print(f'Данные успешно сохранены в файл {file_name_expott}.json\n')
-        elif num_exp == '3':
+        elif num_exp == '2':
             export_data.csv_to_csv(file_name_expott)
             print(f'Данные успешно сохранены в файл {file_name_expott}.csv\n')
-
-
-
-
-            #path = 'phonebook.csv'
-            '''
-            def import_csv(path_to_import_csv_file): 
-                data = []        
-                with open(path_to_import_csv_file, "r", newline='', encoding='utf-8') as file:
-                    
-                    file_reader = csv.DictReader(file, delimiter = ",") 
-                    for row in file_reader:
-                        g = list(row.values())
-                        d = g[0].split(';')
-                        print(d, type(d))
-                        data.append(g)     
-                return data
-            arr1 = import_csv(path)
-            
-            for i in range(0, len(arr1)):
-                k = arr1[i]
-                
-                for i in range(len(k)):
-                    j = k[i]
-                    
-                    with open(file_name_expott, "a", encoding='utf-8') as fil:
-                        csv_fil = csv.writer(fil, delimiter=',')
-                        csv_fil.writerow(j.split(';'))
-            print('Данные успешно сохранены')
-        
-        else:
-            print('Пожалуста, введите номер пункта меню: ')
-            enter = input('Нажмите Enter для выхода в меню ')
-
-        enter = input('Нажмите Enter для завершения')
-        '''
 
 
     elif n == '4':
--- export_data.py
+++ export_data.py
@@ -22,31 +22,6 @@ def csv_to_csv(user_file_name, data=csv_to_data()):
             bdd.writerow(i)
 
 
-
-def export_to_xml(user_file_name, data=csv_to_data()):
-    xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
-    xml += '<notes>\n'
-    for row in data:
-        id, name, body, date, time = row
-        xml += '    <note>\n'
-        xml += '        <ID>{}</ID>\n' \
-            .format(id)
-        xml += '        <NAME>{}</NAME>\n' \
-            .format(name)
-        xml += '        <BODY>{}</BODY>\n' \
-            .format(body)
-        xml += '        <DATE>{}</DATE>\n' \
-            .format(date)
-        xml += '        <TIME>{}</TIME>\n' \
-            .format(time)
-        xml += '    </note>\n'
-    xml += '</notes>'
-    with open(f'{user_file_name}.xml', 'w', encoding='utf-8') as page:
-
-        page.write(xml)
-    return data
-
-
 # работает
 def csv_to_json(jsonFilePath, csvFilePath='database.csv'):
 
@@ -54,9 +29,6 @@ def csv_to_json(jsonFilePath, csvFilePath='database.csv'):
     with open(f"{jsonFilePath}.json", 'w', encoding='utf-8') as jsonf:
         jsonString = json.dumps(jsonArray, indent=4, ensure_ascii=False)
         jsonf.write(jsonString)
-        '''
-        Если sure_ascii имеет значение true (по умолчанию), 
-        в выводе гарантированно будут экранированы все входящие символы, отличные от ASCII. 
-        Если sure_ascii имеет значение false, эти символы будут выводиться как есть.  
-        '''
+        
+        
 
